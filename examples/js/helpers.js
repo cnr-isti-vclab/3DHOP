aae3854d3102ac83b4cc96da1fd8552d01398df9
Marco Callieri <callieri@isti.cnr.it>
2018-12-11 17:15:48 +0100

    added js file with useful functions

---
+++ examples/js/helpers.js
@@ -0,0 +1,65 @@
+/*
+3DHOP - 3D Heritage Online Presenter
+Copyright (c) 2014-2018, Visual Computing Lab, ISTI - CNR
+All rights reserved.    
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+//------------------------------------------------------
+//  HSB space to RGB [0-1]
+//------------------------------------------------------
+function hsvToRgb(h, s, v) {
+    var i,f,p,q,t;
+
+	// Achromatic (grey)
+    if(s == 0) return [ v, v, v ];
+
+    h /= 60; // sector 0 to 5
+    i = Math.floor(h);
+    f = h - i; // factorial part of h
+    p = v * (1 - s);
+    q = v * (1 - s * f);
+    t = v * (1 - s * (1 - f));
+
+    switch(i) {
+        case 0:
+		    return [ v, t, p ];
+            break;
+        case 1:
+			return [ q, v, p ];		
+            break;
+        case 2:
+			return [ p, v, t ];
+            break;
+        case 3:
+			return [ p, q, v ];
+            break;
+        case 4:
+			return [ t, p, v ];		
+            break;
+        default: // case 5:
+			return [ v, p, q ];		
+    }
+}
+
+
+//-----------------------------------------
+// creates a copy of a pure JSON structure
+//-----------------------------------------
+function jsonCopy(src) {
+  return JSON.parse(JSON.stringify(src));
+}
